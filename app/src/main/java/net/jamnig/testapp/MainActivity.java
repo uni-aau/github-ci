bf95a3313f3dab8336dcfec16c9313a460bccfae
djamn <david@jamnig.net>
2024-01-12 13:16:03 +0100

    readded main activity code

--- app/src/main/java/net/jamnig/testapp/MainActivity.java
+++ app/src/main/java/net/jamnig/testapp/MainActivity.java
@@ -2,6 +2,13 @@ package net.jamnig.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
