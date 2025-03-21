4fa5e0a6fa11acbb513636d4b355ab00a642f8b4
djamn <david@jamnig.net>
2025-03-21 19:25:10 +0100

    downgraded sonar scanner version to fix crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("jacoco")
-    id("org.sonarqube") version "6.0.1.5171"
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 android {
